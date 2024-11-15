63fcb97f36368160d319d6401547f11f47f69e53
yingSYYing <167300384+yingSYYing@users.noreply.github.com>
2024-11-15 10:31:33 +0800

    Update Business_profile.py

--- main/Business_profile.py
+++ main/Business_profile.py
@@ -3,28 +3,28 @@ import pages.Profile_Form as pf
 st.title("Business Profile")
 
 st.subheader("Business")
-st.write(pf.business_type)
+st.write(st.session_state.business_type)
 
 st.subheader("Industry")
-st.write(pf.industry_option)
+st.write(st.session_state.industry_option)
 
 st.subheader("Products")
-st.write(pf.product)
+st.write(st.session_state.product)
 
 st.subheader("Location")
-st.write(pf.updated_location)
+st.write(st.session_state.updated_location)
 
 st.subheader("Demographics")
-st.write(pf.demographics)
+st.write(st.session_state.demographics)
 
 st.subheader("Target Finish Date")
-st.write(pf.target_date)
+st.write(st.session_state.target_date)
 
 st.subheader("Strengths")
-st.write(pf.strength_bus)
+st.write(st.session_state.strength_bus)
 
 st.subheader("Finance")
-st.write(pf.finance)
+st.write(st.session_state.finance)
 
 st.subheader("Initial Funds")
-st.write(pf.intiial_funds)
+st.write(st.session_state.intiial_funds)
