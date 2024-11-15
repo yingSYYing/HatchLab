852743a2136eb93ddf69545706644b3a4a7c5078
yingSYYing <167300384+yingSYYing@users.noreply.github.com>
2024-11-15 14:24:20 +0800

    Update Business_profile.py

--- main/Business_profile.py
+++ main/Business_profile.py
@@ -2,6 +2,25 @@ import streamlit as st
 import pages.Profile_Form as pf
 st.title("Business Profile")
 
+if 'business_type' not in st.session_state:
+        st.session_state.business_type = "N/A"
+if 'product' not in st.session_state:
+        st.session_state.product = "N/A"
+if 'industry_option' not in st.session_state:
+        st.session_state.industry_option = "N/A"
+if 'location' not in st.session_state:
+        st.session_state.location = "N/A"
+if 'demographics' not in st.session_state:
+        st.session_state.demographics = "N/A"
+if 'target_date' not in st.session_state:
+        st.session_state.target_date = "N/A"
+if 'strength_bus' not in st.session_state:
+        st.session_state.strength_bus = "N/A"
+if 'finance' not in st.session_state:
+        st.session_state.finance = "N/A"
+if 'initial_funds' not in st.session_state:
+        st.session_state.initial_funds = 0
+
 st.subheader("Business")
 st.write(st.session_state.business_type)
 
